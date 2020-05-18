d612a676bb36d194e4128d63055e7b524a181314
Miah <sowhoyou@gmail.com>
2020-05-17 23:23:52 -0400

    Dirty Code Changes
    More Dirty Ass Code Changes - Code Cleanup Coming Soon!

--- clip.php
+++ clip.php
@@ -26,41 +26,22 @@ class Clip {
         $this->auth = new Auth();
         $this->clipLog = new ClipTracker();
         $this->clip_vars = array(
-            'pre_message' => '',
-            'error_message' => '',
-            'clip_link' => '',
-            'clip_link_data' => '',
-            'clip_verified' => false,
-            'clip_link_was_null' => false,
-            'channel_exists_in_db' => false,
             'client_clip_api_headers' => '',
-        );
-        $this->strings = array(
-            'channel_id_not_numerical' => 'Client Side Error! - Invalid Channel ID!',
-            'channel_offline' => 'Clipping is not possible at this time! If the stream just went live or restarted, try again later.',
-            'discord_webhook_url_prefix' => 'https://discordapp.com/api/webhooks/',
-            'first_thumb_msg_client' => 'Warning: First Time Use & Thumbnail Generation was Enabled. This API Works! Try Clipping again! If the clip doesn\'t work, disable thumbnail generation, because your bot doesn\'t support it. ',
-            'first_thumb_msg_logs' => 'First Time Use + Thumb Gen Message Sent',
-            'header_auth_prefix' => 'Authorization: Bearer ',
-            'header_client_id' => 'Client-ID: 8utds3yp9mk0mebqy4lirqnnyzalkl',
-            'missing_channel' => 'Client Side Error! - Channel ID is Missing!',
-            'missing_oauth' => 'Client Side Error! - OAuth Token is Missing!',
-            'null_clip_issue' => 'Twitch API Error! - Unknown RIP',
-            'oauth_expired' => 'OAuth Token Expired! - Update your Clip Command at: https://www.sowhoyou.com/clipcommand/',
-            'twitch_clip_api_url' => 'https://api.twitch.tv/helix/clips?broadcaster_id=',
-            'twitch_clip_url_prefix' => 'https://clips.twitch.tv/',
-            'twitch_clip_verify_api_url' => 'https://api.twitch.tv/helix/clips?id=',
-            'twitch_error_invalid_oauth' => 'Must provide a valid Client-ID or OAuth token',
-            'twitch_error_stream_offline' => 'Clipping is not possible for an offline channel.',
+            'clip_link_data' => '',
+            'clip_link_was_null' => false,
+            'clip_link' => '',
+            'clip_verified' => false,
+            'error_message' => '',
         );
         $this->client_auth = array(
-            'version' => '0',
             'channel' => '',
-            'webhook' => '',
-            'oauth' => '',
-            'format' => false,
             'encrypt' => false,
+            'format' => false,
+            'oauth' => '',
+            'sender' => '',
             'verify' => false,
+            'version' => '0',
+            'webhook' => '',
         );
     }
 
@@ -81,43 +62,35 @@ class Clip {
             $this->client_auth['format'] = (isset($_GET['f']) ? ((strcasecmp($_GET['f'], '1') === 0) ? true : false) : false);
             $this->client_auth['encrypt'] = (isset($_GET['e']) ? ((strcasecmp($_GET['e'], '1') === 0) ? true : false) : false);
             $this->client_auth['verify'] = (isset($_GET['v']) ? ((strcasecmp($_GET['v'], '1') === 0) ? true : false) : false);
+            $this->client_auth['sender'] = (isset($_GET['u']) ? $_GET['u'] : '');
         }
     }
 
     function swy_var_validation() {
         if ($this->client_auth['encrypt'] === true) {
             $this->client_auth['oauth'] = !empty($this->client_auth['oauth']) ? $this->swy_decrypt($this->client_auth['oauth']) : '';
-            $this->client_auth['webhook'] = !empty($this->client_auth['webhook']) ? ($this->strings['discord_webhook_url_prefix'] . $this->swy_decrypt($this->client_auth['webhook'])) : '';
+            $this->client_auth['webhook'] = !empty($this->client_auth['webhook']) ? ('https://discordapp.com/api/webhooks/' . $this->swy_decrypt($this->client_auth['webhook'])) : '';
         } else {
-            $this->client_auth['webhook'] = !empty($this->client_auth['webhook']) ? ($this->strings['discord_webhook_url_prefix'] . $this->client_auth['webhook']) : '';
+            $this->client_auth['webhook'] = !empty($this->client_auth['webhook']) ? ('https://discordapp.com/api/webhooks/' . $this->client_auth['webhook']) : '';
         }
-        if (empty($this->client_auth['oauth']) || empty($this->client_auth['channel'])) {
-            die(empty($this->client_auth['oauth']) ? $this->strings['missing_oauth'] : $this->strings['missing_channel']);
-        }
-        if (!is_numeric($this->client_auth['channel'])) {
-            die($this->strings['channel_id_not_numerical']);
-        }
-        $this->clip_vars['client_clip_api_headers'] = array(
-            $this->strings['header_auth_prefix'] . $this->client_auth['oauth'],
-            $this->clip_vars['header_client_id'],
-        );
-        $this->clip_vars['channel_exists_in_db'] = $this->clipLog->exists($this->client_auth['channel']);
-    }
 
-    function swy_clip_processing() {
-        $this->getClipLink();
-        $this->checkForErrors();
-        $this->nullCheck();
-        $this->verifyClip();
-        $this->logClipToDatabase();
-        $this->sendClipLinkReply();
+        if (empty($this->client_auth['oauth']) || empty($this->client_auth['channel'])) {
+            die('Client Side Error! - ' . empty($this->client_auth['oauth']) ? 'OAuth Token is Missing!' : 'Channel ID is Missing!');
+        }
+
+        if (!is_numeric($this->client_auth['channel'])) {
+            die('Client Side Error! - Invalid Channel ID!');
+        }
+
+        $this->clip_vars['client_clip_api_headers'] = array(
+            'Authorization: Bearer ' . $this->client_auth['oauth'],
+            'Client-ID: ' . $this->auth->twitch['clip_cmd_client_id'],
+        );
     }
 
     function getClipLink() {
         $curl = curl_init();
-        $twitch_api_parms = $this->client_auth['channel'];
-        $twitch_api_url = $this->strings['twitch_clip_api_url'];
-        curl_setopt($curl, CURLOPT_URL, ($twitch_api_url . $twitch_api_parms));
+        curl_setopt($curl, CURLOPT_URL, ('https://api.twitch.tv/helix/clips?broadcaster_id=' . $this->client_auth['channel']));
         curl_setopt($curl, CURLOPT_HTTPHEADER, $this->clip_vars['client_clip_api_headers']);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_POST, 1);
@@ -127,21 +100,34 @@ class Clip {
             if (isset($json['data']['0']['id'])) {
                 $this->clip_vars['clip_link'] = $json['data']['0']['id'];
             } else {
-                if (isset($json['message'])) {
-                    if (strcasecmp($json['message'], $this->strings['twitch_error_invalid_oauth']) === 0) {
-                        $this->clip_vars['error_message'] = $this->strings['oauth_expired'];
-                    } elseif (strcasecmp($json['message'], $this->strings['twitch_error_stream_offline']) === 0) {
-                        $this->clip_vars['error_message'] = $this->strings['channel_offline'];
-                    } else {
-                        if (!empty($json['message'])) {
-                            $this->clip_vars['error_message'] = $json['message'];
-                            $this->swy_error_log(__FUNCTION__ . '1', $this->clip_vars['error_message']);
-                        }
-                    }
-                } else { $this->swy_error_log(__FUNCTION__ . '2', json_encode($json));}
+                $this->process_clip_json_data($json);
             }
-        } else { $this->swy_error_log(__FUNCTION__ . '3', curl_error($curl));}
+        } else {
+            $this->swy_error_log(__FUNCTION__ . '1', curl_error($curl));
+        }
         curl_close($curl);
+    }
+
+    function process_clip_json_data($json) {
+        if (isset($json['message'])) {
+            $update_message = 'Update your Clip Command at: https://www.sowhoyou.com/clipcommand/';
+
+            if (strcasecmp($json['message'], 'Must provide a valid Client-ID or OAuth token') === 0) {
+                $this->clip_vars['error_message'] = 'OAuth Token Expired! - ' .  $update_message;
+
+            } elseif ((strcasecmp($json['message'], 'Invalid OAuth token') === 0) || (strcasecmp($json['message'], 'Missing scope: clips:edit') === 0)) {
+                $this->clip_vars['error_message'] = 'Invalid OAuth Token! - ' .  $update_message;
+
+            } elseif (strcasecmp($json['message'], 'Clipping is not possible for an offline channel.') === 0) {
+                $this->clip_vars['error_message'] = 'Clipping is not possible at this time! If the stream just went live or restarted, try again later.';
+
+            } elseif (!empty($json['message'])) {
+                $this->clip_vars['error_message'] = $json['message'];
+                $this->swy_error_log(__FUNCTION__ . ' 1', $this->clip_vars['error_message']);
+            }
+        } else {
+            $this->swy_error_log(__FUNCTION__ . ' 2', json_encode($json));
+        }
     }
 
     function checkForErrors() {
@@ -150,6 +136,7 @@ class Clip {
         }
     }
 
+    /** Attempt to fix Twitch's Unknown Error */
     function nullCheck() {
         if (empty($this->clip_vars['clip_link'])) {
             if ($this->clip_vars['clip_link_was_null'] === false) {
@@ -158,14 +145,14 @@ class Clip {
                 $this->clip_vars['clip_link_was_null'] = true;
                 $this->nullCheck();
             } else {
-                $this->swy_error_log(__FUNCTION__ . '1', $this->clip_vars['error_message']);
-                die($this->strings['null_clip_issue']);
+                $this->swy_error_log(__FUNCTION__ . ' 1', $this->clip_vars['error_message']);
+                die('Twitch API Error! - Unknown Error! - RIP');
             }
         }
     }
 
     function verifyClip() {
-        if ($this->client_auth['verify'] && $this->clip_vars['channel_exists_in_db']) {
+        if ($this->client_auth['verify']) {
             $this->verifyClipLinkLoop();
         } else {
             $this->verifyClipLink();
@@ -190,7 +177,7 @@ class Clip {
 
     function verifyClipLink() {
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, ($this->strings['twitch_clip_verify_api_url'] . $this->clip_vars['clip_link']));
+        curl_setopt($curl, CURLOPT_URL, ('https://api.twitch.tv/helix/clips?id=' . $this->clip_vars['clip_link']));
         curl_setopt($curl, CURLOPT_HTTPHEADER, $this->clip_vars['client_clip_api_headers']);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($curl);
@@ -198,29 +185,27 @@ class Clip {
             $json = json_decode($result, true);
             $this->verify_clip_data = (isset($json['data']['0']['id'])) ? $json['data']['0']['id'] : '';
         } else {
-            $this->swy_error_log(__FUNCTION__ . '1', curl_error($curl));
+            $this->swy_error_log(__FUNCTION__ . ' 1', curl_error($curl));
         }
         curl_close($curl);
     }
 
     function logClipToDatabase() {
-        if ($this->client_auth['verify'] && !$this->clip_vars['channel_exists_in_db']) {
-            $this->clip_vars['pre_message'] = $this->strings['first_thumb_msg_client'];
-            $this->swy_error_log(__FUNCTION__ . '1', $this->strings['first_thumb_msg_logs']);
-        }
-        $this->clipLog->log($this->client_auth['channel'], $this->clip_vars['clip_link']);
+        /** Log to My Discord */
+        $this->clipLog->log($this->client_auth['channel'], $this->clip_vars['clip_link'], $this->client_auth['verify'], $this->client_auth['sender']);
+        /** Log to Client Discord if Webhook is given */
         if (!empty($this->client_auth['webhook'])) {
-            $this->clipLog->sendClientWebhook($this->client_auth['webhook'], $this->client_auth['channel'], $this->clip_vars['clip_link'], $this->client_auth['format']);
+            $this->clipLog->sendClientWebhook($this->client_auth['webhook'], $this->client_auth['channel'], $this->clip_vars['clip_link'], $this->client_auth['format'], $this->client_auth['sender']);
         }
     }
 
     function sendClipLinkReply() {
-        die($this->clip_vars['pre_message'] . $this->strings['twitch_clip_url_prefix'] . $this->clip_vars['clip_link']);
+        die('https://clips.twitch.tv/' . $this->clip_vars['clip_link']);
     }
 
     function swy_decrypt($string) {
         $output = false;
-        $encrypt_method = "AES-128-OFB";
+        $encrypt_method = $this->auth->encryption['method'];
         $key = hash('sha256', $this->auth->encryption['key']);
         $iv = substr(hash('sha256', $this->auth->encryption['iv']), 0, 16);
         $data = str_replace(array('-', '_'), array('+', '/'), $string);
@@ -234,11 +219,123 @@ class Clip {
         error_log('>>> ' . $this->client_auth['channel'] . ' >> ' . $location . ' > ' . $error, 0);
     }
 
+    function disable_cloudbot_thumb_gen() {
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+        $user_agent_found = false;
+        /** Check if the bot is a cloud bot */
+        if ($this->client_auth['verify'] === true) {
+            $cloud_bots = $this->auth->ccmd_api_cloudbots();
+            foreach ($cloud_bots as $bot) {
+                if (stripos($user_agent, $bot) !== false) {
+                    // $this->swy_error_log('Thumbnail Generation Disabled', $bot);
+                    $this->client_auth['verify'] = false;
+                    $user_agent_found = true;
+                    break;
+                }
+            }
+        }
+        /**  Check if the bot is not a cloud bot */
+        if ($this->client_auth['verify'] === true) {
+            $non_cloud_bots = $this->auth->ccmd_api_non_cloudbots();
+            foreach ($non_cloud_bots as $ok_bot) {
+                if (stripos($user_agent, $ok_bot) !== false) {
+                    $user_agent_found = true;
+                    break;
+                }
+            }
+        }
+        /** Log all non registered bots that have thumb gen enabled */
+        if ($this->client_auth['verify'] === true && $user_agent_found === false) {
+            file_put_contents('agents.log', "\r\n" . $user_agent, FILE_APPEND);
+            $this->swy_error_log('Thumbnail Generation', 'Bot Logged');
+        }
+    }
+
+    function prevent_clips_from_browsers() {
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+        $found_browser = false;
+        $found_chatbot = false;
+        $browser_found = '';
+        $chatbot_found = '';
+        /** Known Browser Agents */
+        $web_browsers = $this->auth->ccmd_api_known_webbrowsers();
+        /** Known Chatbots */
+        $valid_bots = $this->auth->ccmd_api_valid_chatbots();
+        if (!empty($user_agent)) {
+            /** Check Browser Agents */
+            foreach ($web_browsers as $browser) {
+                if (strpos($user_agent, $browser) !== false) {
+                    $found_browser = true;
+                    $browser_found .= ' ' . $browser;
+                }
+            }
+            /** Check Chatbots */
+            foreach ($valid_bots as $bot) {
+                if (strpos($user_agent, $bot) !== false) {
+                    $found_chatbot = true;
+                    $chatbot_found .= ' '. $bot;
+                }
+            }
+        }
+        /** Pre Log Message */
+        $msg = $this->client_auth['channel'] . ' >>> ';
+        /** Browser Deny Message */
+        $deny_msg = 'Sorry, but clips can only be made by valid Twitch Chatbots! ';
+        $deny_msg .= 'If you feel this message was an error, please contact SOWHOYOUdotCOM';
+        /** Logs & Errors for Browser Agents and Chatbots */
+        if ($found_browser && $found_chatbot) {
+            /** Agents & Bot */
+            $msg .= 'Bot Allowed with Browser >> ' . $chatbot_found . ' & Agents >' . $browser_found;
+            // error_log($msg, 0);
+        } elseif ($found_browser) {
+            /** Agents Only */
+            file_put_contents('blocked.log', "\r\nBrowser Blocked >>> Channel: " . $this->client_auth['channel'] . ' >> ' . $user_agent, FILE_APPEND);
+            /** Send Deny Msg */
+            $msg .= 'Browser Blocked >> ' . $browser_found;
+            die($deny_msg);
+        } elseif ($found_chatbot) {
+            /** Bot Only */
+            $msg .= 'Bot Allowed >> ' . $chatbot_found;
+            // error_log($msg, 0);
+        } else {
+            /** Unknown */
+            if (!empty($user_agent)) {
+                $msg .= 'Unknown Chatbot >> ' . $user_agent;
+                error_log($msg, 0);
+            }
+        }
+    }
+
+    function deny_banned_users() {
+        $banned_users = $this->auth->ccmd_api_banned_users();
+        foreach ($banned_users as $user) {
+            if (strcasecmp($this->client_auth['channel'], $user) === 0) {
+                error_log($user . ' - Banned Access Denied!', 0);
+                die('API Access Denied!');
+            }
+        }
+    }
+
+    function swy_clip_processing() {
+        $this->getClipLink();
+        $this->checkForErrors();
+        $this->nullCheck();
+        $this->verifyClip();
+        $this->logClipToDatabase();
+        $this->sendClipLinkReply();
+    }
+
 }
 
 $clip = new Clip();
+/** Primary Functions */
 $clip->swy_url_processing();
 $clip->swy_var_validation();
+$clip->deny_banned_users();
+/** Secondary Functions */
+$clip->prevent_clips_from_browsers();
+$clip->disable_cloudbot_thumb_gen();
+/** Final Functions */
 $clip->swy_clip_processing();
 
 ?>
